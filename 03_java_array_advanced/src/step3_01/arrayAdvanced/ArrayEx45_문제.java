ed310bc98f137660e4f1d8022b21d19d7dc31b06
15_web_ysh <15_web_ysh@302-13>
2023-05-10 16:22:49 +0900

    23-05-10

--- 03_java_array_advanced/src/step3_01/arrayAdvanced/ArrayEx45_문제.java
+++ 03_java_array_advanced/src/step3_01/arrayAdvanced/ArrayEx45_문제.java
@@ -3,7 +3,7 @@ package step3_01.arrayAdvanced;
 import java.util.Random;
 import java.util.Scanner;
 
-//# 빙고 Ver1(플레이어 1명)
+//# 빙고 Ver1(플레이어 1명)             
 
 public class ArrayEx45_문제 {
 
@@ -15,25 +15,35 @@ public class ArrayEx45_문제 {
 		int maxNum = 5;
 		int[][] bingo = new int[maxNum][maxNum];
 		int[][] mark = new int[maxNum][maxNum];	
-		int[] temp = new int[maxNum*maxNum];
+		int[] temp = new int[maxNum*maxNum];	//25
 		int size = 50;	
 		int win = 0;
 		
 		// 중복을 제거한 랜덤숫자를 임시 저장
 		for (int i=0; i < temp.length; i++) {
-			temp[i] = ran.nextInt(size) + 1;
-			for (int j = 0; j < i; j++) {
+			temp[i] = ran.nextInt(size) + 1;	//1~51
+			for (int j = 0; j < i; j++) {		//
 				if (temp[i] == temp[j]) {
-					i--;
+					i--;								
 				}
 			}
 		}
 		
+		for (int i = 0; i < temp.length; i++) {
+			temp[i] = ran.nextInt(size) + 1;
+			for (int j = 0; j < i; j++) {
+				if(temp[i] == temp[j]) {
+					i--;
+					
+				}
+			}
+		}
+		System.out.println(Arrays);
 		// 중복을 제거한 랜덤숫자를 빙고판에 대입
-		int k = 0;
+		int k = 0;										
 		for (int i = 0; i < maxNum; i++) {
 			for (int j = 0; j < maxNum; j++) {
-				bingo[i][j] = temp[k];
+				bingo[i][j] = temp[k];			
 				k++;
 			}
 		}
@@ -42,11 +52,11 @@ public class ArrayEx45_문제 {
 		
 		while (true) {
 			System.out.println("=============== BINGO GAME ===============");
-			System.out.print("\t");
-			for (int i = 0; i < maxNum; i++) {
-				System.out.print(i + "\t");
+			System.out.print("\t");						//tab 	
+			for (int i = 0; i < maxNum; i++) {			
+				System.out.print(i + "\t");	
 			}
-			System.out.println("\n");
+			System.out.println("\n");										
 			for (int i = 0; i < maxNum; i++) {
 				System.out.print(i + "\t");
 				for (int j = 0; j < maxNum; j++) {
